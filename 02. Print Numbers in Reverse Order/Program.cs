154e747e69a336e4df544e20f25544e598d786b9
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-21 04:04:31 +0300

    Add files via upload

---
+++ 02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] myArray = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    myArray[i] = number;
+}
+
+for (int i = myArray.Length - 1; i >= 0; i--)
+{
+    Console.Write(myArray[i] + " ");
+}
