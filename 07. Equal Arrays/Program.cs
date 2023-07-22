499674ef92bb594550790fcb8159bdeb8e56f181
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-22 04:27:27 +0300

    Add files via upload

---
+++ 07. Equal Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+int[] array1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] array2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int sum = 0;
+bool areEqual = true;
+
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i] != array2[i])
+    {
+        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+        areEqual = false;
+        break;
+    }
+
+    sum += array1[i];
+}
+
+if (areEqual)
+{
+    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+}
