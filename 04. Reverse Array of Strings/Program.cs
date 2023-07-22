0e506a6b8314b5457014a375528d4f22f45b1ebd
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-22 04:24:28 +0300

    Add files via upload

---
+++ 04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,10 @@
+﻿var elements = Console.ReadLine().Split(' ').ToArray();
+
+for (int i = 0; i < elements.Length / 2; i++)
+{
+    var tempElement = elements[i];
+    elements[i] = elements[elements.Length - 1 - i];
+    elements[elements.Length - 1 - i] = tempElement;
+}
+
+Console.WriteLine(string.Join(" ", elements));
