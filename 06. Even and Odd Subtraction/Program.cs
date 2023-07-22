050ef8366b4ced390a5ada59ae4da8ab5c2b726e
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-22 04:26:29 +0300

    Add files via upload

---
+++ 06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int evenSum = 0;
+int oddSum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int currentNumber = numbers[i];
+
+    if (currentNumber % 2 == 0)
+    {
+        evenSum += currentNumber;
+    }
+    else
+    {
+        oddSum += currentNumber;
+    }
+}
+
+Console.WriteLine(evenSum - oddSum);
